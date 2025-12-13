8c7cc99a88a9138fb8dc53f5d1297ad243c095db
trihung987 <51873366+trihung987@users.noreply.github.com>
2025-12-13 19:15:57 +0700

    remove init local model

--- src/main/java/hcmute/vina/vectorsearchservice/service/EmbeddingServiceImpl.java
+++ src/main/java/hcmute/vina/vectorsearchservice/service/EmbeddingServiceImpl.java
@@ -65,7 +65,6 @@ public class EmbeddingServiceImpl implements EmbeddingService{
     private CourseEmbeddingRepository courseEmbeddingRepository;
 
     //Tạm thời sử dụng OpenAI thay vì model mpnet-base-v2 nên sẽ không init
-    @PostConstruct
     public void init() throws ModelNotFoundException, MalformedModelException, IOException {
         Criteria<String, float[]> criteria = Criteria.builder()
                 .optApplication(ai.djl.Application.NLP.TEXT_EMBEDDING)
